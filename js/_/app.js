8077a9f2d96d002e83a5bfaa7120eaa6945ab467
Matita <matitald@gmail.com>
2014-12-24 20:27:55 +0100

    cleaning code in app.js

--- js/_/app.js
+++ js/_/app.js
@@ -1,34 +1,5 @@
 $(function() {
 
-	
-
-	function onPromptClick() {
-		var $this = $(this),
-			url = $this.attr('data-link').substr(1),
-			flow = new RedditFlow(url);
-
-		$this.addClass('selected');
-		$(document.body).addClass('viewing-stories');
-
-		window.location = '#';
-
-		$('.stories').text('loading stories...');
-
-		flow.older(function(response) {
-			var comments = response[1].data.children.map(function(comment) {
-					var data = comment.data;
-					console.debug('data', data);
-					data.permalink = url + data.id;
-					return data;
-				});
-			console.debug('comments', comments);
-			var html = comments.map(Tmpl.story).join('');
-			
-			$('.stories').html(html);
-		});
-	}
-
-
 	$('body')
 		.on('click', 'a', function() {
 			var href = $(this).attr('href');
@@ -37,13 +8,7 @@ $(function() {
 				route();
 				return false;
 			}
-		})
-		/*.on('click', '.prompt', onPromptClick)
-		.on('click', '.back-to-prompts', function(e) {
-			$('body').removeClass('viewing-stories');
-			$('.prompt.selected').removeClass('selected');
-			e.stopPropagation();
-		});*/
+		});
 
 
 	function route() {
